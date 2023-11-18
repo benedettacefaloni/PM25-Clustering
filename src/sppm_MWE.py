c6de91215013297d9107b89f93d7f89b497e27b3
Flo-Wo <florianwolf1710@t-online.de>
2023-11-18 12:47:58 +0100

    MWE with all arguments

---
+++ src/sppm_MWE.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import rpy2.robjects as ro
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects import pandas2ri
+
+# Minimal Working Example to call the sppm function in Python
+
+# Activate automatic conversion of pandas objects to R objects
+# pandas2ri.activate()
+
+# Import the drpm package
+drpm = rpackages.importr("drpm")
+
+# Simplify the call to the drpm_fit function
+drpm_fit = drpm.drpm_fit
+
+nobs = 100
+
+# Response variable (y)
+y = np.random.normal(0, 1, nobs)
+
+y_test = [1, 1, 1, 1, 1, 1]
+v = ro.FloatVector(y_test)
+m = ro.r["matrix"](v, nrow=2)
+print("m = ")
+print(type(m))
+print(m)
+
+# Spatial coordinates (s1, s2)
+s = np.random.uniform(0, 10, nobs * 2)
+s_float = ro.FloatVector(s)
+s_coords = ro.r["matrix"](s_float, nrow=int(nobs), ncol=2)
+
+r_data_test = {
+    "y": m,
+    "s_coords": s_coords,
+    "M": 2,
+    "starting_alpha": 0.1,
+    "unit_specific_alpha": False,
+    "time_specific_alpha": False,
+    "alpha_0": False,
+    "eta1_0": False,
+    "phi1_0": False,
+    "modelPriors": ro.FloatVector([0, 100**2, 1, 1, 1, 1]),
+    "alphaPriors": ro.r["matrix"](ro.FloatVector([1, 1]), nrow=1),
+    "simpleModel": 0,
+    "theta_tau2": ro.FloatVector([0, 2]),
+    "SpatialCohesion": 4,
+    "cParms": ro.FloatVector([0, 1, 2, 1]),
+    "mh": ro.FloatVector([0.5, 1, 0.1, 0.1, 0.1]),
+    "verbose": True,
+    "draws": 10000,
+    "burn": 100,
+    "thin": 10,
+}
+
+# Call the simplified drpm_fit function
+result = drpm_fit(**r_data_test)
+
+# Convert the result to a Python object if needed
+# print(result.r_repr())
+# result_python = pandas2ri.ri2py(result)
