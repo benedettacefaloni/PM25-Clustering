b4c5506862565c94631d73ebd2a34e1b941a0fd1
Flo-Wo <florianwolf1710@t-online.de>
2023-12-07 20:28:26 +0100

    add takeaways

--- src/analyze_drpm.py
+++ src/analyze_drpm.py
@@ -25,6 +25,16 @@ from utils.visualize import (
     trace_plots,
 )
 
+"""
+Take aways:
+- First experiment: using the parameters presented by page et al. -> huge Problems with too big clusters
+    - nearly all stations are in the same cluster
+    - best performing clusters tested for diff. combination of hyperparams
+- 2nd Experiment: lowered value of sigma prior --> make cluster smaller
+    - takes way longer
+    - TODO: best 3 models taken for fine-tuning test
+"""
+
 priors = {
     # params as stated in the original drpm paper of page et al.
     "paper_params": {
