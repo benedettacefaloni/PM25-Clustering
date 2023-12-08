1a0d6bbb3119fddc754763389d52ac5f1645d629
Flo-Wo <florianwolf1710@t-online.de>
2023-12-08 12:08:39 +0100

    2nd experiment

--- src/analyze_drpm.py
+++ src/analyze_drpm.py
@@ -33,6 +33,15 @@ Take aways:
 - 2nd Experiment: lowered value of sigma prior --> make cluster smaller
     - takes way longer
     - TODO: best 3 models taken for fine-tuning test
+
+TODO:
+* Further experiments:
+    - use three/4 modifications for each of the three models and make a smaller table
+* Plots: For one model of each category?
+    - Trace plots 
+    - Plot weekly errors over time (waic, mse, etc.)
+    - plot cluster kpis over time
+    - image of clustering for a time stamp to compare the results
 """
 
 priors = {
